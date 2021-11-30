57fad9c608d4bf0d3cdbd2360fd2b507e065f5f4
107103120 <107103120@mail.aeust.edu.tw>
2021-11-30 11:33:55 +0800

    Change School Number

--- dht.ino
+++ dht.ino
@@ -1,9 +1,9 @@
 #include <dht.h>     
-#define dht_dpin A0 	           //�w�q�T���n�qPin A0 �i��  
+#define dht_dpin A0 	           //DHT 11 類比輸入Pin A0   
 dht DHT;   
 
 String a;
-String Sch_no = "123";
+String Sch_no = "120";
 
 void setup(){  
   Serial.begin(9600);   
